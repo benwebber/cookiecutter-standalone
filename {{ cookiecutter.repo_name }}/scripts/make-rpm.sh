7c116fb9630db29c5c830a0e3a97275cbef6e09d
Ben Webber <benjamin.webber@gmail.com>
2015-07-14 22:34:44 -0400

    Fix build scripts for hyphenated project names
    Establish naming convention:
    
      * project: common name of package, may include hyphens.
      * package: importable Python package name

--- {{ cookiecutter.repo_name }}/scripts/make-rpm.sh
+++ {{ cookiecutter.repo_name }}/scripts/make-rpm.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+PROJECT=$1
 
 mkdir -p build/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
 cd build/rpmbuild
 
-cp "../../rpm/${1}.spec" SPECS/
+cp "../../rpm/${PROJECT}.spec" SPECS/
 cp ../../scripts/* SOURCES/
 cp ../../dist/*.tar.gz SOURCES/
 
-rpmbuild --define "_topdir $(pwd)" -ba "SPECS/${1}.spec"
+rpmbuild --define "_topdir $(pwd)" -ba "SPECS/${PROJECT}.spec"
 
 find . -name '*.rpm' -exec cp {} ../../dist/ \;
--- {{ cookiecutter.repo_name }}/scripts/make-standalone.sh
+++ {{ cookiecutter.repo_name }}/scripts/make-standalone.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+PROJECT=$1
+PACKAGE=$2
+WHEEL=$3
 
 # Install package and dependencies to build directory.
-pip install "dist/${2}" --no-compile --target build/standalone
+pip install "dist/${WHEEL}" --no-compile --target build/standalone
 
 # Make runnable zip file.
-zip -jr "build/${1}.zip" "${1}/__main__.py"
+zip -jr "build/${PROJECT}.zip" "${PACKAGE}/__main__.py"
 
 # Install packages to zip file.
-cd build/standalone && zip -gr "../${1}.zip" .
+(cd build/standalone && zip -gr "../${PROJECT}.zip" .)
 
 # Make zip file executable.
-echo '#!/usr/bin/env python' | cat - "build/${1}.zip" > "build/${1}.pyz"
-install -m 755 "build/${1}.pyz" "dist/${1}"
+echo '#!/usr/bin/env python' | cat - "build/${PROJECT}.zip" > "build/${PROJECT}.pyz"
+install -m 755 "build/${PROJECT}.pyz" "dist/${PROJECT}"
