332d882d96aa1f8a13d39d5494906a8010080709
Ben Webber <benjamin.webber@gmail.com>
2015-12-20 19:47:01 -0500

    Clean up standalone build script
      * separate build directory and output directories
      * add platform suffix to executable

--- {{ cookiecutter.repo_name }}/scripts/make-standalone.sh
+++ {{ cookiecutter.repo_name }}/scripts/make-standalone.sh
@@ -3,18 +3,27 @@ set -euo pipefail
 IFS=$'\n\t'
 
 PROJECT=$1
-PACKAGE=$2
-WHEEL=$3
+VERSION=$2
+PACKAGE=$3
+WHEEL=$4
+PLATFORM="$(python -c 'import setuptools; print(setuptools.distutils.util.get_platform())')"
+ARTIFACT="${PROJECT}-${VERSION}-${PLATFORM}"
+BUILD_DIR="build/standalone.${PLATFORM}/build"
+OUTPUT_DIR="build/standalone.${PLATFORM}/output"
+
+# Make separate directories to install package and collect output.
+mkdir -p "${BUILD_DIR}"
+mkdir -p "${OUTPUT_DIR}"
 
 # Install package and dependencies to build directory.
-pip install "dist/${WHEEL}" --no-compile --target build/standalone
+pip install "dist/${WHEEL}" --no-compile --target "${BUILD_DIR}"
 
 # Make runnable zip file.
-zip -jr "build/${PROJECT}.zip" "${PACKAGE}/__main__.py"
+zip -jr "${OUTPUT_DIR}/${ARTIFACT}.zip" "${PACKAGE}/__main__.py"
 
 # Install packages to zip file.
-(cd build/standalone && zip -gr "../${PROJECT}.zip" .)
+(cd "${BUILD_DIR}" && zip -gr "../output/${ARTIFACT}.zip" .)
 
 # Make zip file executable.
-echo '#!/usr/bin/env python' | cat - "build/${PROJECT}.zip" > "build/${PROJECT}.pyz"
-install -m 755 "build/${PROJECT}.pyz" "dist/${PROJECT}"
+echo '#!/usr/bin/env python' | cat - "${OUTPUT_DIR}/${ARTIFACT}.zip" > "${OUTPUT_DIR}/${ARTIFACT}"
+install -m 755 "${OUTPUT_DIR}/${ARTIFACT}" "dist/${ARTIFACT}"
